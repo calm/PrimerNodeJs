16cb0cb18e06a0b6afa8368f2d51fe434a470c30
calm <limonggi@gmail.com>
2015-09-07 16:39:41 -0430

    cuarto commit con el no existes aplicado

--- node/main.js
+++ node/main.js
@@ -1,10 +1,22 @@
 var http = require('http');
 
+
+function noExistes404(request, response){
+	response.writeHead(404, {"Content-type":""});
+	response.write("no existes...");
+	response.end();
+}
+
+
 function onRequest(request, response){
 	console.log("comenzo el onrequest" + request.url);
-	/*response.writeHead(200, {"Content-type":""});
-	response.write("aqui va some data.. I've got you by the horns");
-	response.end();*/
+	if (request.url == '/') {
+		response.writeHead(200, {"Content-type":""});
+		response.write("aqui va some data.. I've got you by the horns");
+		response.end();/**/
+	}else {
+		noExistes404(request, response);
+	}
 
 //comentario
 
@@ -13,12 +25,7 @@ function onRequest(request, response){
 //probando
 }
 
-function 404noExistes(request, response){
-	response.writeHead(404, {"Content-type":""});
-	response.write("no existes...");
-	response.end();
-}
 
-
-http.createServer(onRequest).listen(8888);
+var server = http.createServer(onRequest).listen(8888);
 console.log("server corriendo....");
+//server.close();

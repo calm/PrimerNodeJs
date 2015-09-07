cf7a188f687cdaeb603332d709f6d6106f4b9713
calm <limonggi@gmail.com>
2015-09-07 15:53:34 -0430

    primer nodejs commit

---
+++ node/main.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+function onRequest(request, response){
+	console.log("comenzo el onrequest" + request.url);
+	/*response.writeHead(200, {"Content-type":""});
+	response.write("aqui va some data.. I've got you by the horns");
+	response.end();*/
+
+//comentario
+
+}
+
+function 404noExistes(request, response){
+	response.writeHead(404, {"Content-type":""});
+	response.write("no existes...");
+	response.end();
+}
+
+
+http.createServer(onRequest).listen(8888);
+console.log("server corriendo....");
